2199388db3806ce20365d31e2810c39fa737cb03
Charmis Varghese <charmispv@users.noreply.github.com>
2016-05-01 17:15:32 -0400

    New Solution
    VS 2015 Solution created

---
+++ FHIR_Client.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FHIR_Client.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
